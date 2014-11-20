2d0fd4c804e8160c15fe7f49e4541255a470beac
Joe <Joe@Cmptergeekfreak.wi.rr.com>
2014-11-19 22:33:06 -0600

    IO Lab

---
+++ src/lab1/Lab1_1.java
@@ -0,0 +1,39 @@
+
+package lab1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Joe
+ */
+public class Lab1_1 {
+    public static void main(String[] args) throws IOException {
+        String filePath = "src" + File.separatorChar + "lab1.txt";
+        File data = new File(filePath);
+        BufferedReader in = null;
+        try {
+	   in = new BufferedReader(new FileReader(data));
+	   String line = in.readLine();
+	   while(line != null){
+		  System.out.println(line);
+		  line = in.readLine();  // strips out any carriage return chars
+	   }
+	 
+        } catch(IOException ioe) {
+            System.out.println("Houston, we have a problem! reading this file");
+        } finally {
+            try {
+                in.close();
+            } catch(Exception e) {
+                
+            }
+        }
+        
+
+    }
+    
+}
---
+++ src/lab1/readme.txt
@@ -0,0 +1,14 @@
+Lab #1 Instructions
+======================================================
+Create a text file using Notepad++ or your favorite text editor. Place the 
+file in “C:\temp\lab1” if using Windows O/S. If using a Macintosh, place the file 
+in “/Users/<yourUserName>\Documents”. The file name should end in “.txt”. 
+Enter data representing a small mailing list. Format exactly as follows (each 
+group of 3 lines is one record, create at least 3 records): 
+FirstName LastName
+Street Address
+City, State Zipcode
+… now read each record from the file and output to the console. Once you’ve 
+successfully shown that you can read data from a file, append one or two new 
+records using write techniques
+
